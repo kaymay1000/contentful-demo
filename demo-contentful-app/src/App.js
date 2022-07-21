c72101a6a37c66f022063cea6059bd95d40df39b
Kaylee Alvarado <kaylee@assembleinc.com>
2022-07-21 02:34:22 -0700

    successfully requesting all projects

--- demo-contentful-app/src/App.js
+++ demo-contentful-app/src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+// const query = `
+// {
+//   project(id: "4SkcJwNH5fIJEQxvdf6r9B") {
+//     projectTitle
+//     projectDescription {
+//       json
+//     }
+//     projectImagesCollection {
+//       items {
+//         title
+//         description
+//         fileName
+//         url
+//       }
+//     }
+//   }
+// }
+// `;
+
 const query = `
 {
-  project(id: "4SkcJwNH5fIJEQxvdf6r9B") {
-    projectTitle
-    projectDescription {
-      json
-    }
-    projectImagesCollection {
-      items {
-        title
-        description
-        fileName
-        url
+  projectCollection {
+    total
+    items {
+      projectTitle
+      projectDescription {
+        json 
+      }
+      projectImagesCollection {
+        items {
+          description
+          url
+        }
       }
     }
   }
@@ -21,7 +41,8 @@ const query = `
 `;
 
 function App() {
-  const [project, setProject] = useState(null);
+  // const [project, setProject] = useState(null);
+  const [allProjects, setAllProjects] = useState(null);
 
   useEffect(() => {
     window
@@ -29,33 +50,47 @@ function App() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // athenticate the request
+          // authenticate the request
           "Authorization": `Bearer ${process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN}`,
         },
         // send the GraphQL query
-        body: JSON.stringify({query})
+        body: JSON.stringify({ query }),
       })
       .then((response) => response.json())
       .then(({data, errors}) => {
         if (errors) { console.error(errors) }
+
         // rerender the entire component with new data
-        setProject(data.project);
-        console.log('data.project: ', data.project);
-      })
+        // setProject(data.project);
+        setAllProjects(data.projectCollection.items);
+        console.log('data.projectCollection: ', data.projectCollection.items);
+      });
   }, []);
 
   // show a loading screen case the data hasn't arrived yet
-  if (!project) {
-    return "Loading...";
+  if (!allProjects) {
+  // if (!allProjects) {
+    return <p className="loading">Loading...</p>;
+  }
+
+  const renderProjects = () => {
+    return allProjects.map(project => (
+      <div className="project-wrapper" key={project.projectTitle}>
+        <h1 className="project-title">{project.projectTitle}</h1>
+        <img src={project.projectImagesCollection.items[0].url} className="project-cover-image" alt="logo" />
+        <p className="project-desc">{project.projectDescription.json.content[0].content[0].value}</p>
+      </div>
+        
+    ))
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{project.projectTitle}</h1>
-        <img src={project.projectImagesCollection.items[0].url} className="project-cover-image" alt="logo" />
-        <p>{project.projectDescription.json.content[0].content[0].value}</p>
-      </header>
+      <header className="App-header">Interior Design Portfolio</header>
+      {/* <h1>{project.projectTitle}</h1>
+      <img src={project.projectImagesCollection.items[0].url} className="project-cover-image" alt="logo" />
+      <p>{project.projectDescription.json.content[0].content[0].value}</p> */}
+      <div className="projects-wrapper">{renderProjects()}</div>
     </div>
   );
 }
