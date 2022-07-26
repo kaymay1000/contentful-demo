3fe2e96517c2d0b8140ba55ab1a4b5e872cb75a2
Kaylee Alvarado <kaylee@assembleinc.com>
2022-07-25 23:29:52 -0700

    tags rendering

--- demo-contentful-app/src/components/ProjectCard/index.js
+++ demo-contentful-app/src/components/ProjectCard/index.js
@@ -1,13 +1,22 @@
 import './project-card.scss';
 
 const ProjectCard = (project) => {
+  console.log('project: ', project);
   let fields = project.project.fields;
+  let tags = project.project.metadata.tags;
+
 
   return (
     <div className="project-wrapper">
       <h1 className="project-title">{fields.projectTitle}</h1>
       <img src={fields.projectImages[0].fields.file.url} className="project-cover-image" alt="logo"/>
       <p className="project-desc">{fields.projectDescription.content[0].content[0].value}</p>
+      {/* in order to be accessible to the CDA client, tags must be made public when being created in Contentful */}
+      <ul>
+      {tags.map((tag, index) => {
+        return <li className="tag" key={index}>{tag.sys.id}</li>
+      })}
+      </ul>
     </div>
   )
 }
--- demo-contentful-app/src/contentfulClient.js
+++ demo-contentful-app/src/contentfulClient.js
@@ -1,4 +1,6 @@
-  export const contentfulClient = require('contentful').createClient({
+ // the "contentful" package is a JS library that allows interaction with Contentful's Content Delivery API 
+ // and Content Preview API (NOT the Content Management API)
+ export const contentfulClient = require('contentful').createClient({
     space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
     accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
   });
